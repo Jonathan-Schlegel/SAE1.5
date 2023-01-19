454fcb7a4ce798e300de977e7a76b6e6f2a43f53
Jonathan-Schlegel <jacqueschlegel@gmail.com>
2023-01-19 15:57:09 +0100

    19/01/2023

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+#Fichier exécution du programme principale
 from recuperationStockageDonnee import *
+import time
 
-parking="FR_MTP_GARE"
+parkings=['FR_MTP_ANTI','FR_MTP_COME','FR_MTP_CORU','FR_MTP_EURO','FR_MTP_FOCH','FR_MTP_GAMB','FR_MTP_GARE','FR_MTP_TRIA','FR_MTP_PITO','FR_MTP_CIRC','FR_MTP_SABI','FR_MTP_GARC','FR_MTP_MOSS','FR_STJ_SJLC','FR_MTP_MEDC','FR_MTP_OCCI','FR_CAS_VICA','FR_MTP_GA109','FR_MTP_GA250','FR_CAS_CDGA','FR_MTP_ARCE','FR_MTP_POLY']
+"""'FR_MTP_ARCT',"""
+def lancement(heure,echantillonnage):
+    for x in range(round((heure*60)/echantillonnage)):
+        print(x)
+        for parking in parkings:
+            liste=recuperationParkingVoiture(parking)
+            ecritureDonneeVoiture(liste,parking)
+            print(liste)
+        liste=recuperationParkingVelo()
+        print(liste)
+        ecritureDonneeVelo(liste)
+        time.sleep(echantillonnage*60)
 
-liste=recuperationParkingVelo()
-ecritureDonneeVelo(liste)
-liste=recuperationParkingVoiture(parking)
-ecritureDonneeVoiture(liste,parking)
+lancement(24,0.5)
+
+
--- recuperationStockageDonnee.py
+++ recuperationStockageDonnee.py
@@ -6,16 +6,16 @@ import time
 import csv
 
 #Liste station id
-lsitStationID={'001': 'Rue Jules Ferry - Gare Saint-Roch', '002': 'Comédie', '003': 'Esplanade', '004': 'Hôtel de Ville', '005': 'Corum', '006': 'Place Albert 1er - St Charles', '007': 'Foch', '008': 'Halles Castellane', '009': 'Observatoire', '010': 'Rondelet', '011': 'Plan Cabanes', '012': 'Boutonnet', '013': 'Emile Combes', '014': 'Beaux-Arts', '015': 'Les Aubes', '016': 'Antigone centre', '017': 'Médiathèque Emile Zola', '018': "Nombre d'Or", '019': 'Louis Blanc', '020': 'Gambetta', '021': 'Port Marianne', '022': 'Clemenceau', '023': 'Les Arceaux', '024': 'Cité Mion', '025': 'Nouveau Saint-Roch', '026': 'Renouvier', '027': 'Odysseum', '028': 'Saint-Denis', '029': 'Richter', '030': 'Charles Flahault', '031': 'Voltaire', '032': "Prés d'Arènes", '033': 'Garcia Lorca', '034': 'Vert Bois', '035': 'Malbosc', '036': 'Occitanie', '037': 'FacdesSciences', '038': 'Fac de Lettres', '039': 'Aiguelongue', '040': 'Jeu de Mail des Abbés', '041': 'Euromédecine', '042': 'Marie Caizergues', '043': 'Sabines', '044': 'Celleneuve', '045': 'Jardin de la Lironde', '046': 'Père Soulas', '047': 'Place Viala', '048': 'Hôtel du Département', '049': 'Tonnelles', '050': 'Parvis Jules Ferry - Gare Saint-Roch', '051': 'Pont de Lattes - Gare Saint-Roch', '053': 'Deux Ponts - Gare Saint-Roch', '054': 'Providence - Ovalie', '055': "Pérols Etang de l'Or", '056': 'Albert 1er - Cathédrale', '057': 'Saint-Guilhem - Courreau', '059': 'Sud De France'}
+listStationID={'001': 'Rue Jules Ferry - Gare Saint-Roch', '002': 'Comédie', '003': 'Esplanade', '004': 'Hôtel de Ville', '005': 'Corum', '006': 'Place Albert 1er - St Charles', '007': 'Foch', '008': 'Halles Castellane', '009': 'Observatoire', '010': 'Rondelet', '011': 'Plan Cabanes', '012': 'Boutonnet', '013': 'Emile Combes', '014': 'Beaux-Arts', '015': 'Les Aubes', '016': 'Antigone centre', '017': 'Médiathèque Emile Zola', '018': "Nombre d'Or", '019': 'Louis Blanc', '020': 'Gambetta', '021': 'Port Marianne', '022': 'Clemenceau', '023': 'Les Arceaux', '024': 'Cité Mion', '025': 'Nouveau Saint-Roch', '026': 'Renouvier', '027': 'Odysseum', '028': 'Saint-Denis', '029': 'Richter', '030': 'Charles Flahault', '031': 'Voltaire', '032': "Prés d'Arènes", '033': 'Garcia Lorca', '034': 'Vert Bois', '035': 'Malbosc', '036': 'Occitanie', '037': 'FacdesSciences', '038': 'Fac de Lettres', '039': 'Aiguelongue', '040': 'Jeu de Mail des Abbés', '041': 'Euromédecine', '042': 'Marie Caizergues', '043': 'Sabines', '044': 'Celleneuve', '045': 'Jardin de la Lironde', '046': 'Père Soulas', '047': 'Place Viala', '048': 'Hôtel du Département', '049': 'Tonnelles', '050': 'Parvis Jules Ferry - Gare Saint-Roch', '051': 'Pont de Lattes - Gare Saint-Roch', '053': 'Deux Ponts - Gare Saint-Roch', '054': 'Providence - Ovalie', '055': "Pérols Etang de l'Or", '056': 'Albert 1er - Cathédrale', '057': 'Saint-Guilhem - Courreau', '059': 'Sud De France'}
 
 
 #Cette fonction n'a pour but que de recolter les données d'un parkin mis en paramètre. 
 def recuperationParkingVoiture(parking):
         response=requests.get(f"https://data.montpellier3m.fr/sites/default/files/ressources/{parking}.xml")
-        f=open("parkings/response.txt","w",encoding="utf8")
+        f=open("response.txt","w",encoding="utf8")
         f.write(response.text)
         f.close()
-        tree=etree.parse("response.txt")
+        tree= etree.parse("response.txt")
         #Place libre
         for user in tree.xpath("Free"):
             placeLibre=user.text
@@ -61,3 +61,5 @@ def ecritureDonneeVoiture(liste,parking):
     ecriture=csv.writer(fichier,delimiter=",")
     ecriture.writerow(liste)
     fichier.close()
+
+recuperationParkingVoiture("FR_MTP_GARE")
